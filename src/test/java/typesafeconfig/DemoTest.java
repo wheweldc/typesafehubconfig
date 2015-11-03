b0a3eeb4a347845a5032b576a246e53858f77ce6
Desmond Whewell <desmond.whewell@first-utility.com>
2015-11-03 17:04:38 +0000

    Confirm resolution using conf files

--- src/test/java/typesafeconfig/DemoTest.java
+++ src/test/java/typesafeconfig/DemoTest.java
@@ -13,7 +13,7 @@ import com.typesafe.config.ConfigFactory;
 public class DemoTest {
 
     @Test
-    public void testUsingParseResourcesAnySyntaxNoResolutionRequired() {
+    public void testPropertiesUsingParseResourcesAnySyntaxNoResolutionRequired() {
         Config config = ConfigFactory.parseResourcesAnySyntax("noresolution.properties");
 
         assertNotNull(config);
@@ -22,7 +22,7 @@ public class DemoTest {
     }
 
     @Test
-    public void testUsingParseResourcesAnySyntaxResolutionRequired() {
+    public void testPropertiesUsingParseResourcesAnySyntaxResolutionRequired() {
         Config config = ConfigFactory.parseResourcesAnySyntax("resolution.properties");
 
         assertNotNull(config);
@@ -37,6 +37,33 @@ public class DemoTest {
 
         assertEquals("3", resolvedConfig.getValue("four"));
 
+    }
+
+    @Test
+    public void testConfUsingParseResourcesAnySyntaxNoResolutionRequired() {
+        Config config = ConfigFactory.parseResourcesAnySyntax("noresolution.conf");
+    
+        assertNotNull(config);
+        assertFalse(config.isEmpty());
+        assertTrue(config.isResolved());
+    }
+
+    @Test
+    public void testConfUsingParseResourcesAnySyntaxResolutionRequired() {
+        Config config = ConfigFactory.parseResourcesAnySyntax("resolution.conf");
+    
+        assertNotNull(config);
+        assertFalse(config.isEmpty());
+        assertFalse(config.isResolved());
+    
+        Config resolvedConfig = config.resolve();
+    
+        assertNotNull(resolvedConfig);
+        assertFalse(resolvedConfig.isEmpty());
+        assertTrue(resolvedConfig.isResolved());
+    
+        assertEquals("3", resolvedConfig.getString("four"));
+    
     }
 
 }
